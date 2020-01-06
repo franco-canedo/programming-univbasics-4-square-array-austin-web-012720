b520d5b360aad55f8946a61576b812eb1be285d7
Franco Canedo <francanedo1@hotmail.com>
2020-01-06 23:53:31 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,8 +3,8 @@ def square_array(array)
   counter = 0
   
   while counter < array.length do
-    array[counter]**2
-    p array[counter]
+    p array[counter]**2
+    
     counter += 1
   end
 end
